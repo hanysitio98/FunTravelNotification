7774237066bbfa3611fa90ebf816efb43f7382e7
Hany Juliana <64250079+hanysitio98@users.noreply.github.com>
2023-04-13 09:52:36 +0700

    Initial commit

---
+++ src/main/java/id/co/bca/spring/FunTravelNotification/FunTravelNotification/FunTravelNotificationApplication.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.FunTravelNotification.FunTravelNotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunTravelNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunTravelNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/bca/spring/FunTravelNotification/FunTravelNotification/FunTravelNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.FunTravelNotification.FunTravelNotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunTravelNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
